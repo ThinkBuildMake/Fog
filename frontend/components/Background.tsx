ace3711e7724efd7a2d2f01c7ad7f2dad28fd04f
Causality-C <39445369+Causality-C@users.noreply.github.com>
2021-06-20 16:08:40 -0500

    testing husky + lint-staged yay

---
+++ frontend/components/Background.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import backgroundGradient from  "../assets/icon-blue-cloud-gear.png";
+const IndexPage: React.FC = () => {
+    return (
+        <div
+            style={{ background: 'blue', width: 'auto', height: '100vh' }}
+        ></div>
+    )
+}
+export default IndexPage
