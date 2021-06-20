c30b78817a87cc93f82414e1d79519b826523719
Causality-C <39445369+Causality-C@users.noreply.github.com>
2021-06-20 15:11:18 -0500

    Added TS Support and project skeleton

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import {useEffect, useState} from 'react';
+import Link from 'next/link'
+const IndexPage: React.FC = () => {
+
+	return(
+		<h1>Hello World</h1>
+	)
+}
+
+export default IndexPage;
