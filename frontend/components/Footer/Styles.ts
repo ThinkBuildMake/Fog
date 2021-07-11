d4062bc8d867a1f929fe003f961706747e706a62
Michael Chuang <michael.chuang@utexas.edu>
2021-07-10 19:25:18 -0500

    Adding Footer to Web Page

---
+++ frontend/components/Footer/Styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const FooterList = styled.ul`
+    color: #ffffff;
+`
+
+export const FooterLinks = styled.a`
+    color: #ffffff;
+    padding: 10px;
+    text-decoration: none;
+    &:hover {
+        text-decoration: underline;
+    }
+    position: relative;
+    top: -13px;
+`
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '@components/Header/Header'
 import DefaultLayout from '@templates/DefaultLayout'
+import Footer from '@components/Footer/Footer'
 
 const IndexPage: React.FC = () => {
     return (
         <DefaultLayout>
             <Header />
+            <Footer />
         </DefaultLayout>
     )
 }
