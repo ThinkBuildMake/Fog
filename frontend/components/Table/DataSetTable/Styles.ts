a73101ebc00c0cfe8c373212201c1b102b1cfac7
Causality-C <39445369+Causality-C@users.noreply.github.com>
2021-07-23 12:08:06 -0500

    Updated Table Styles to Be More Responsive

--- frontend/components/Table/DataSetTable/Styles.ts
+++ frontend/components/Table/DataSetTable/Styles.ts
@@ -3,27 +3,52 @@ export const Table = styled.table`
     border: 1px solid black;
     width: 100%;
     border-collapse: collapse;
+    overflow-y: auto;
+    max-height: calc(100vh - 6rem - 300px);
+    display: block;
+    thead,
+    tbody,
+    tr {
+        display: block;
+        width: 100%;
+    }
     th {
         font-size: 36px;
         padding-top: 16px;
+        position: sticky;
+        top: 0;
+        z-index: 1;
+    }
+    thead {
+        position: sticky;
+        top: 0;
+        z-index: 1;
+        background-color: white;
     }
     td {
         font-size: 24px;
     }
     tr {
         border-bottom: 1px solid black;
-        height: 70px;
+        min-height: 60px;
+        display: flex;
+        align-items: center;
+        padding-top: 5px;
+        padding-bottom: 5px;
+    }
+    tbody tr:last-child {
+        border-bottom: none;
     }
     * {
         font-family: Poppins;
     }
     .left {
-        width: 80%;
+        width: 65vw;
         text-align: left;
         padding-left: 20px;
     }
     .right {
-        width: 20%;
+        width: 30vw;
         text-align: right;
         padding-right: 20px;
     }
