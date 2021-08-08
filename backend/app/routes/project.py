48c8df83b266b21c2e56a2776f47ed409961632f
Eralp Orkun <eaorkun@gmail.com>
2021-08-08 00:19:54 -0500

    checkin resource funtion

--- backend/app/routes/project.py
+++ backend/app/routes/project.py
@@ -59,3 +59,41 @@ def delete_project(id):
         return jsonify(message="Project ID not found", status=404), 404
     project.delete()
     return jsonify(message="Deletion Successful", status=200), 200
+
+@project.route("/<string:id>/checkout", methods=['POST'])
+@jwt_required()
+def checkout_resource(id):
+    if request.is_json:
+        # Get Json from Request
+        req_json = request.get_json()
+        project = Project.objects(id=id).first()
+        hardware_set = project['hardware_set']
+        if req_json['hardware_id'] in hardware_set:
+            hardware_set[req_json['hardware_id']] += req_json['qty']
+        else:
+            hardware_set[req_json['hardware_id']] = req_json['qty']
+        project.save()
+
+        return jsonify(message="Checked out Hardware Set Successfully", status=200), 200
+    else:
+        return jsonify(message="Request needs to be JSON format", status=400), 400  # change this error code
+
+@project.route("/<string:id>/checkin", methods=['POST'])
+@jwt_required()
+def checkin_resource(id):
+    if request.is_json:
+        # Get Json from Request
+        req_json = request.get_json()
+        project = Project.objects(id=id).first()
+        hardware_set = project['hardware_set']
+        if req_json['hardware_id'] in hardware_set:
+            if hardware_set[req_json['hardware_id']] < req_json['qty']:
+                return jsonify(message="Not enough hardware resources to perform action", status=400), 400  # change this error code
+            else:
+                hardware_set[req_json['hardware_id']] -= req_json['qty']
+        else:
+            return jsonify(message="Hardware resource not found", status=404), 404  # change this error code
+        project.save()
+        return jsonify(message="Checked in Hardware Set Successfully", status=200), 200
+    else:
+        return jsonify(message="Request needs to be JSON format", status=400), 400  # change this error code
