827d641cdbf19cfc674180ac9ee34a12e6a50278
eaorkun <eaorkun@gmail.com>
2021-10-23 17:30:59 -0500

    added refreshing tokens using cookies

--- backend/app/routes/hardware.py
+++ backend/app/routes/hardware.py
@@ -1,8 +1,10 @@
 import hashlib
 import requests
-
+from datetime import datetime
+from datetime import timedelta
+from datetime import timezone
 from flask import Blueprint, jsonify, request
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, get_jwt, set_access_cookies
 import jwt
 import uuid
 from mongoengine import ValidationError, NotUniqueError, OperationError
@@ -27,6 +29,24 @@ def handle_not_unique_error(error):
 @hardware.errorhandler(OperationError)
 def handle_operation_error(error):
     return jsonify(message=str(error), status=400 ), 400
+
+# Using an `after_request` callback, we refresh any token that is within 30
+# minutes of expiring. Change the timedeltas to match the needs of your application.
+@hardware.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            set_access_cookies(response, access_token)
+        return response
+    except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original response
+        return response
+
+
 
 @hardware.route("/", methods=['POST'])
 @jwt_required()
--- backend/app/routes/project.py
+++ backend/app/routes/project.py
@@ -1,16 +1,20 @@
 from flask import Blueprint, request, jsonify
+from datetime import datetime
+from datetime import timedelta
 
 # Blueprints modularize code
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt, get_jwt_identity, create_access_token, set_access_cookies
 from mongoengine import ValidationError, NotUniqueError, OperationError
 
 from app.customfuncs.customfunctions import get_essential_json
 from app.database.models import Project
 
 from datetime import timezone
-import datetime
 
 project = Blueprint('project', __name__)
+
+
+
 
 
 # Define Exceptions
@@ -29,7 +33,21 @@ def handle_operation_error(error):
     return jsonify(message=str(error), status=400), 400
 
 
-
+# Using an `after_request` callback, we refresh any token that is within 30
+# minutes of expiring. Change the timedeltas to match the needs of your application.
+@project.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            set_access_cookies(response, access_token)
+        return response
+    except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original response
+        return response
 
 @project.route("/", methods=['POST'])
 @jwt_required()
